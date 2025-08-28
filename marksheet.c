bd1cffa2385ee1e4baa07bc3beeaa3d6d54ca18d
sathu1509 <sathagathullathalapathi@gmail.com>
2025-08-27 23:55:16 -0700

    Add files via upload

---
+++ marksheet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+printf ("\t\t\t\t\t\t\tBCA-A")
+printf ("\n______________________________________________");
+printf ("\nname:ravi\t\t\t\t\t\t\treg no:0001");
+printf ("\n______________________________________________");
+float mark [5]={78,81,53,64,51,};
+float total =mark[0]+mark[1]+mark[2]+mark[3]+mark[4];
+float average =total\5;
+printf ("\ntamil\t\t\t\t\t:%5.2f",mark[0]);
+printf ("\nenglish\t\t\t\t\t:%5.2f",mark[1]);
+printf ("\nmaths\t\t\t\t\t:%5.2f",mark[2]);
+printf ("\science\t\t\t\t\t:%5.2f",mark[3]);
+printf ("\nsocial\t\t\t\t\t:%5.2f",mark[4]);
+printf ("\n_______________________________________________");
+printf ("\ntotal\t\t\t\t\t:%f\t\t\tavg:%f",total,average);
+
+getch ();
+return 0;
+
+
+	
