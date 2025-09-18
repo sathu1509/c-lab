689cef6af7b87ca39f25d353616b094d85111fb6
sathu1509 <sathagathullathalapathi@gmail.com>
2025-09-18 00:45:06 -0700

    Add files via upload

---
+++ positive integer.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+int n,i;
+printf("enter a positive integar:");
+scanf("%d",&n);
+if (n<=0){
+printf("please enter a positive integer greater than 0.\n");
+return 1;
+}
+printf("\nodd numbers from 1 to %d are:\n",n);
+for (i=1;i<=n;i+=2){
+
+printf("%d",i);
+}
+printf("\n");
+return 0;
+}
+
